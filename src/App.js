ed6bf62f7a2ae4198ddaa8907a4b4d1e3d9025b9
Gabriel <gabrielgomesportela@gmail.com>
2022-09-22 15:55:04 -0300

    Adicionada a funcionalidade para sortear palavra

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import palavras from "./palavras"
+import { useState } from "react"
 import "./css/reset.css"
 import "./css/style.css"
 import forca0 from "./assets/img/forca0.png"
@@ -8,34 +8,73 @@ import forca3 from "./assets/img/forca3.png"
 import forca4 from "./assets/img/forca4.png"
 import forca5 from "./assets/img/forca5.png"
 import forca6 from "./assets/img/forca6.png"
-
-function Letra(props) {
-    return (
-        <div className="letra">{props.letra.toUpperCase()}</div>
-    )
-}
+import palavras from "./palavras"
 
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const [letraSelecionada, setLetraSelecionada] = useState("")
+    const [arrayPalavra, setArrayPalavra] = useState([])
+    const [errosImagem, setErrosImagem] = useState(forca0)
+    const [erros, setErros] = useState(0)
+    const [palavraSorteada, setPalavraSorteada] = useState([])
+
+    function selecionarLetra(letra, index) {
+        if (palavraSorteada.includes(letra)) {
+            alert("tem")
+        } else {
+            const quantidadeErros = erros + 1
+            console.log("erros: " + quantidadeErros)
+            setErros(quantidadeErros)
+            if (quantidadeErros === 0) {
+                setErrosImagem(forca0)
+            } else if (quantidadeErros === 1) {
+                setErrosImagem(forca1)
+            } else if (quantidadeErros === 2) {
+                setErrosImagem(forca2)
+            } else if (quantidadeErros === 3) {
+                setErrosImagem(forca3)
+            } else if (quantidadeErros === 4) {
+                setErrosImagem(forca4)
+            } else if (quantidadeErros === 5) {
+                setErrosImagem(forca5)
+            } else if (quantidadeErros === 6) {
+                setErrosImagem(forca6)
+                console.log("perdeu")
+            }
+        }
+    }
+
+    function sortearPalavra() {
+        const novaPalavra = palavras.sort(indicator)[0].split('')
+        setPalavraSorteada(novaPalavra)
+        const palavraoculta = novaPalavra.map(l => " _")
+        setArrayPalavra(palavraoculta)
+        console.log(arrayPalavra)
+    }
+
+    function indicator() {
+        return Math.random() - 0.5;
+    }
+
     return (
         <div className="app">
             <div className="topo">
                 <div className="forca">
-                    <img src={forca0} />
+                    <img src={errosImagem} />
                 </div>
                 <div className="direita">
                     <div className="sortear">
-                        <button>Escolher Palavra</button>
+                        <button onClick={sortearPalavra}>Escolher Palavra</button>
                     </div>
-                    <div className="palavra"></div>
+                    <div className="palavra">{arrayPalavra}</div>
                 </div>
             </div>
-            <div className="alfabeto">
-                {alfabeto.map((a, index) => <Letra key={index} letra={a}/>)}
+            <div className="letras">
+                {alfabeto.map((a, index) => <button className="letra inicial" onClick={() => selecionarLetra(a, index)} key={index}>{a.toUpperCase()}</button>)}
             </div>
             <div className="palpite">
                 <p>Já sei a palavra!</p>
-                <input></input>
+                <input disabled={false}></input>
                 <button>Chutar</button>
             </div>
         </div>
